05cb396db8533759ff26c80a2ac53ea669cd8998
Animesh <animesh.ranchi91@gmail.com>
2020-06-26 18:25:09 +0530

    cors solved

--- src/app.js
+++ src/app.js
@@ -4,15 +4,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('../src/db/mongoose')
 
+
 const userRoute = require('../src/routes/user');
 const taskRouter = require('../src/routes/task');
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.json());
 app.use(userRoute);
 app.use(taskRouter);
-app.use(cors());
+
 
 module.exports = app;
