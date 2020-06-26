b408c66eb069e893cc0c0be0447e78bda2a1d91b
Animesh <animesh.ranchi91@gmail.com>
2020-06-26 15:14:31 +0530

    cors enabled

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors');
 require('../src/db/mongoose')
 
 const userRoute = require('../src/routes/user');
@@ -9,5 +9,6 @@ const taskRouter = require('../src/routes/task');
 app.use(express.json());
 app.use(userRoute);
 app.use(taskRouter);
+app.use(cors());
 
 module.exports = app;
